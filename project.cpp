da0880c97039716de40796981c078f44b69fbe55
M.R.B <112229335+MrTheBelal@users.noreply.github.com>
2023-12-07 11:12:39 +0330

    HW  #4 - 2

--- project.cpp
+++ project.cpp
@@ -2,53 +2,17 @@
 using namespace std;
 
 main() {
-    float num1 , num2 , sum ;
-    int method ;
-    char op;
-    cout << "please say your methos to calculate  (1 or 2): " ;
-    cin >> method ;
-    if (method==1) {
-        cout << "please enter number 1: " ;
-        cin >> num1 ;
-        cout << "please enter number 2: " ;
-        cin >> num2 ;
-        sum = num1 + num2 ;
-        cout << "output for (+) is : " << sum << "\n";
-        sum = num1 - num2 ;
-        cout << "output for (-) is : " << sum << "\n";
-        sum = num1 * num2 ;
-        cout << "output for (*) is : " << sum << "\n";
-        sum =(int) num1 % (int) num2 ;
-        cout << "output for (%) is : " << sum << "\n";
-    }
-    else if (method==2){
-        cout << "please enter number 1: " ;
-        cin >> num1 ;
-        cout << "please enter number 2: " ;
-        cin >> num2 ;
-        cout << "please enter operator (-,+,*,%): " ;
-        cin >> op ;
-        switch (op) {
-        case '+':
-            sum = num1 + num2;
-            break;
-        case '-':
-            sum = num1 - num2;
-            break;
-        case '*':
-            sum = num1 * num2;
-            break;
-        case '%':
-            sum =(int) num1 % (int) num2;
-            break;
-        default:
-            cout << "ERROR - there is no operator called : " << op << "\n" ;
-            break;
-        }
-        cout << "output for (" << op << ") is : " << sum << "\n";
-    }
-    else {
-        cout << "you'r method is not registered on this program! \n" ;
-    }
-    system("pause") ;
+    int x,y,z,sum1,sum2;
+    float sum;
+    cout << "Please Enter x: ";
+    cin >> x ;
+    cout << "Please Enter y: ";
+    cin >> y ;
+    cout << "Please Enter Z: ";
+    cin >> z ;
+    sum1 = (x-y) * (x+z);
+    sum2 =((x+y+z)*(x+y+z)) + 1;
+    sum = (float) sum1 / sum2;
+    cout << "the output is: " << sum << "\n";
+    system("pause");
 }
